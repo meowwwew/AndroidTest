2ff17c9c4f79ca310c51bdada5646be1740b185b
meowwwew <meoweweww228@gmail.com>
2026-01-24 16:53:21 +0300

    Add number change action for button

--- app/src/main/java/Alexandra/Chuvakina/MainActivity.kt
+++ app/src/main/java/Alexandra/Chuvakina/MainActivity.kt
@@ -1,6 +1,9 @@
 package Alexandra.Chuvakina
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val rollButton: Button=findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        }
+
+    }
+    private fun rollDice() {
+        val dice= Dice(6)
+        val diceRoll=dice.roll()
+        val rollTextView: TextView=findViewById(R.id.textView2)
+        rollTextView.text=diceRoll.toString()
+    }
+    class Dice(val numSides: Int){
+        fun roll(): Int {
+            return(1..numSides).random()
         }
     }
 }
+
+
